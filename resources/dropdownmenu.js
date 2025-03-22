f82f62821110ea6c30054ed1e77a7a5940580291
Sam Wilson <sam@samwilson.id.au>
2025-03-22 18:34:01 +0800

    Close the page menu when the VE edit link is clicked
    Otherwise it stays open after the page has been opened for editing.
    
    Change-Id: Icb4a208250881e0225f786f9f90818b819192009

--- resources/dropdownmenu.js
+++ resources/dropdownmenu.js
@@ -7,4 +7,13 @@ for ( const freoMenu of document.querySelectorAll( '.skin-freo-menu' ) ) {
 			freoMenu.toggleAttribute( 'data-skin-freo-menu-open', false );
 		}
 	} );
+
+	// Close the menu when clicking the VE edit link, because it doesn't reload
+	// the page (unlike all other links in the menu).
+	const veEdit = freoMenu.querySelector( '#ca-ve-edit' );
+	if ( veEdit ) {
+		veEdit.addEventListener( 'click', () => {
+			freoMenu.toggleAttribute( 'data-skin-freo-menu-open', false );
+		} );
+	}
 }
