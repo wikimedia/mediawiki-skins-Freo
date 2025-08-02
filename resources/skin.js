3dd1413d9f4e7830becb18660c5c52f328566552
Sam Wilson <sam@samwilson.id.au>
2025-08-02 20:32:33 +0800

    Add typeahead search widget
    Change-Id: I25f3043aaa32a32442b501eec239b3ab0bfe1bb0

--- resources/skin.js
+++ resources/skin.js
@@ -1,1 +1,25 @@
 require( './dropdownmenu.js' );
+const { App, restSearchClient, urlGenerator } = require( 'mediawiki.skinning.typeaheadSearch' );
+const Vue = require( 'vue' );
+
+const searchForm = document.querySelector( '.skin-freo-searchform-wrapper form' );
+const searchInput = document.querySelector( '.skin-freo-searchform-wrapper input[name="search"]' );
+const urlGeneratorInstance = urlGenerator( mw.config.get( 'wgScript' ) );
+const restClient = restSearchClient( mw.config.get( 'wgScriptPath' ) + '/rest.php', urlGeneratorInstance );
+const props = {
+	prefixClass: 'skin-freo-',
+	id: searchForm.id,
+	autocapitalizeValue: searchInput.getAttribute( 'autocapitalize' ),
+	autofocusInput: searchInput === document.activeElement,
+	action: searchForm.getAttribute( 'action' ),
+	searchAccessKey: searchInput.getAttribute( 'accessKey' ),
+	showThumbnail: true,
+	restClient,
+	urlGenerator: urlGeneratorInstance,
+	searchTitle: searchInput.getAttribute( 'title' ),
+	searchPlaceholder: searchInput.getAttribute( 'placeholder' ),
+	searchQuery: searchInput.value,
+	showEmptySearchRecommendations: true,
+	showDescription: true
+};
+Vue.createMwApp( App, props ).mount( '.skin-freo-searchform-wrapper' );
